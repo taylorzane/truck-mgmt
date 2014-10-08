8aad98bd148015588c6ca0b0e01e8d409efd8630
Taylor Glaeser <taylor@Haley-Skeros-MacBook-Pro.local>
2014-10-08 17:20:26 -0500

    ad

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,16 +1,38 @@
 class ContactsController < ApplicationController
-  def new
-    @contact = Contact.new
-  end
-
   def create
-    @contact = Contact.new(params[:contact])
+    @contact = Contact.new(contact_params)
     @contact.request = request
-    if @contact.deliver
-      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+    if @contact.save
+      flash.now[:notice] = 'User was created successfully'
     else
-      flash.now[:error] = 'Cannot send message.'
-      render :new
+      flash.now[:error] = 'Cannot create user.'
+      render action: 'new'
     end
   end
+
+
+  private
+  ## Strong Parameters 
+  def contact_params
+    params.require(:user).permit(:name, :pass)
+  end
+
 end
+
+
+=begin
+class UsersController < ApplicationController
+  ## ...
+  def create
+    @user = User.new(user_params) ## Invoke user_params method
+    if @user.save
+      redirect_to @user, notice: 'User was successfully created.' 
+    else
+      render action: 'new'
+    end       
+  end
+  ## ... 
+
+end 
+
+=end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,4 @@
 class Contact < ActiveRecord::Base
-  params.require(:contact).permit(:name, :pass)
+  
   validates_presence_of :name, :pass
 end
